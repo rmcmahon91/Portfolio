29a7f89e7f7d2a7d7197b28c1974018ab9bddb23
Ryan McMahon <ryan.mcmahon4@gmail.com>
2020-07-29 10:56:42 -0400

    new upload

---
+++ index.js
@@ -0,0 +1,18 @@
+var PORT = process.env.PORT || 5000;
+var express = require("express")
+var app= express();
+
+var PORT = process.env.PORT || 3000;
+
+var app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+  
+  app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+  });
